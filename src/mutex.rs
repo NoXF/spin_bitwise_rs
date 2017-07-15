038e50803528fb8641b44afc72d093f5891abc4a
Andrey Cizov <acizov@gmail.com>
2017-07-15 10:46:17 +0100

    RW

--- src/mutex.rs
+++ src/mutex.rs
@@ -7,7 +7,7 @@ use core::ops::{Drop, Deref, DerefMut};
 
 //use std::ops::Shl;
 //use std::ops::BitAnd;
-//use std::ops::AddAssign;
+//use std::ops::AddAssign;;
 
 //type LockType = usize;
 const READERS_MAX: usize = 15;
@@ -21,20 +21,27 @@ pub struct Mutex<T: ? Sized>
     data: UnsafeCell<T>,
 }
 
-pub struct MutexGuard<'a, T: ? Sized + 'a>
+pub struct ReadMutexGuard<'a, T: ? Sized + 'a>
+{
+    lock: &'a Arc<AtomicUsize>,
+    data: &'a T,
+    idx: usize,
+}
+
+pub struct WriteMutexGuard<'a, T: ? Sized + 'a>
 {
     lock: &'a Arc<AtomicUsize>,
     data: &'a mut T,
     idx: usize,
-//    mutex: &'a Mutex<T>
 }
 
 unsafe impl<T: ? Sized + Send> Sync for Mutex<T> {}
+
 unsafe impl<T: ? Sized + Send> Send for Mutex<T> {}
 
 impl<T> Mutex<T>
 {
-//    #[cfg(feature = "const_fn")]
+    //    #[cfg(feature = "const_fn")]
     pub fn new(lock: Arc<AtomicUsize>, user_data: T) -> Mutex<T>
     {
         Mutex {
@@ -46,6 +53,14 @@ impl<T> Mutex<T>
 
 fn atomic_wait(lock: &Arc<AtomicUsize>, idx: usize) -> usize {
     lock.fetch_or(bitmask_wait(idx), Ordering::SeqCst)
+}
+
+fn atomic_lock(lock: &Arc<AtomicUsize>, idx: usize) -> usize {
+    lock.fetch_or(bitmask_lock(idx) | bitmask_wait(idx), Ordering::SeqCst)
+}
+
+fn atomic_unlock(lock: &Arc<AtomicUsize>, idx: usize) -> usize {
+    lock.fetch_xor(bitmask_both(idx), Ordering::SeqCst)
 }
 
 const fn bitmask_wait(id: usize) -> usize {
@@ -68,24 +83,23 @@ impl<T: ? Sized> Mutex<T>
     
     fn set_wait(&self, idx: usize) -> usize {
         atomic_wait(&self.lock, idx)
-//        self.lock.fetch_or(bitmask_wait(idx), Ordering::SeqCst)
     }
     
     fn set_lock(&self, idx: usize) -> usize {
-        self.lock.fetch_or(bitmask_lock(idx) | bitmask_wait(idx), Ordering::SeqCst)
+        atomic_lock(&self.lock, idx)
     }
     
-//    fn set_unlock(&self, idx: usize) -> usize {
-//        self.lock.fetch_xor(bitmask_both(idx), Ordering::SeqCst)
-//    }
+    fn get_reader_id(&self) -> usize {
+        let r: usize = random();
+        return r % READERS_MAX
+    }
     
     fn writer_mask(&self) -> usize {
         return bitmask_both(WRITER_IDX);
     }
     
     pub fn obtain_reader_lock(&self) -> usize {
-        let mut idx : usize = random();
-        idx = idx % READERS_MAX;
+        let mut idx = self.get_reader_id();
         
         let mut prev_state = self.set_wait(idx);
         
@@ -93,10 +107,8 @@ impl<T: ? Sized> Mutex<T>
             if prev_state & bitmask_wait(idx) == 0 {
                 break
             } else {
+                idx = self.get_reader_id();
                 cpu_relax();
-                
-                idx = random();
-                idx = idx % READERS_MAX;
             }
             
             prev_state = self.set_wait(idx);
@@ -108,7 +120,7 @@ impl<T: ? Sized> Mutex<T>
             } else {
                 prev_state = self.get()
             }
-    
+            
             cpu_relax();
         }
         
@@ -127,7 +139,7 @@ impl<T: ? Sized> Mutex<T>
             if a & 0xFFFFFFFF == 0 {
                 break;
             }
-    
+            
             cpu_relax();
         }
         loop {
@@ -141,55 +153,60 @@ impl<T: ? Sized> Mutex<T>
         WRITER_IDX
     }
     
-//    fn data(&mut self) -> &mut T  {
-//        unsafe { &mut *self.data.get() }
-//    }
-    
-    pub fn lock_reader(&self) -> MutexGuard<T>
+    pub fn lock_reader(&self) -> ReadMutexGuard<T>
     {
-        MutexGuard {
+        ReadMutexGuard {
             idx: self.obtain_reader_lock(),
-//            mutex: &mut self,
             lock: &self.lock,
             data: unsafe { &mut *self.data.get() },
-//            id: lock_id
         }
     }
     
-    pub fn lock_writer(&self) -> MutexGuard<T>
+    pub fn lock_writer(&self) -> WriteMutexGuard<T>
     {
-        MutexGuard {
+        WriteMutexGuard {
             idx: self.obtain_writer_lock(),
-//            mutex: &mut self,
             lock: &self.lock,
             data: unsafe { &mut *self.data.get() },
         }
     }
-    
-//    pub fn unlock(&mut self, id: usize) {
-//
-//    }
 }
 
-impl<'a, T: ? Sized> Deref for MutexGuard<'a, T>
-{
-    type Target = T;
-    fn deref<'b>(&'b self) -> &'b T { &*self.data }
+macro_rules! define_deref_for {
+    ($cls:path) => (
+        impl<'a, T: ? Sized> Deref for ($cls)
+        {
+            type Target = T;
+            fn deref<'b>(&'b self) -> &'b T { &*self.data }
+        }
+    )
 }
 
-impl<'a, T: ? Sized> DerefMut for MutexGuard<'a, T>
-{
-    fn deref_mut<'b>(&'b mut self) -> &'b mut T {
-        &mut *self.data
-    }
+macro_rules! define_deref_mut_for {
+    ($cls:path) => (
+        impl<'a, T: ? Sized> DerefMut for ($cls)
+        {
+            fn deref_mut<'b>(&'b mut self) -> &'b mut T {
+                &mut *self.data
+            }
+        }
+    )
 }
 
-impl<'a, T: ? Sized> Drop for MutexGuard<'a, T>
-{
-    /// The dropping of the MutexGuard will release the lock it was created from.
-    fn drop(&mut self)
-    {
-        self.lock.fetch_xor(bitmask_both(self.idx), Ordering::SeqCst);
-//        self.mutex.set_unlock(self.idx);
-    }
+macro_rules! define_drop_for {
+    ($cls:path) => (
+        impl<'a, T: ? Sized> Drop for ($cls)
+        {
+            fn drop(&mut self)
+            {
+                atomic_unlock(self.lock, self.idx);
+            }
+        }
+    )
 }
+
+define_deref_for!(ReadMutexGuard<'a, T>);
+define_deref_for!(WriteMutexGuard<'a, T>);
+define_deref_mut_for!(WriteMutexGuard<'a, T>);
+define_drop_for!(ReadMutexGuard<'a, T>);
+define_drop_for!(WriteMutexGuard<'a, T>);
